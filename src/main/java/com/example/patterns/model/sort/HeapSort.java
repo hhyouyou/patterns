db7839074b3f8c6d2047f982765f3c5a5b762bc0
dongjingxi <dongjingxi@ruigushop.com>
2020-04-29 11:19:54 +0800

    feat: 堆排序

---
+++ src/main/java/com/example/patterns/model/sort/HeapSort.java
@@ -0,0 +1,67 @@
+package com.example.patterns.model.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author dong jing xi
+ * @date 2020/4/28 21:24
+ **/
+public class HeapSort {
+
+
+    public static void main(String[] args) {
+
+        int[] arrays = {9, 2, 5, 1, 3, 2, 9, 5, 2, 1, 8};
+
+        buildMaxHeap(arrays);
+        int length = arrays.length;
+        for (int i = length - 1; i > 1; i--) {
+            swap(arrays, 0, i);
+            maxHeapify(arrays, 0, i);
+        }
+
+        System.out.println(Arrays.toString(arrays));
+
+    }
+
+    // 建堆
+    public static void buildMaxHeap(int[] arrays) {
+        for (int i = (arrays.length / 2) + 1; i >= 0; i--) {
+            maxHeapify(arrays, i, arrays.length);
+        }
+    }
+
+    // 维护最大堆
+    public static void maxHeapify(int[] array, int i, int length) {
+
+        // 左右子节点在数组中下标
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        // 根节点和两个子节点中最大的那个元素的下标
+        int max = i;
+        if (left < length) {
+            if (array[left] > array[max]) {
+                max = left;
+            }
+        }
+        if (right < length) {
+            if (array[right] > array[max]) {
+                max = right;
+            }
+        }
+
+        if (max != i) {
+            swap(array, i, max);
+            maxHeapify(array, max, length);
+        }
+
+    }
+
+
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+}
